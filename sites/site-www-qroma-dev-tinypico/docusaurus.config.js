224aca3991e1efaa831b94e9df7e1da771b8dcb3
Albert Boehmler <ajboehmler@gmail.com>
2023-04-22 08:16:02 -0400

    Configuring site for github pages deploy

--- sites/site-www-qroma-dev-tinypico/docusaurus.config.js
+++ sites/site-www-qroma-dev-tinypico/docusaurus.config.js
@@ -18,8 +18,9 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'qromatech', // Usually your GitHub org/user name.
+  projectName: 'qroma-dev-tinypico', // Usually your repo name.
+  trailingSlash: false,
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
